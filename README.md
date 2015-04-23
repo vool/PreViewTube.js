d6bd047f8ab7812f2afb31250748cddea5cf9e41
Keith <keith@vool.ie>
2015-04-24 00:11:49 +0100

    Initial commit - for reals

---
+++ README.md
@@ -0,0 +1,53 @@
+# PreViewTube.js
+A small jQuery plugin for animating YouTube thumbnails.
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+<script src="path/to/jquery.min.js"></script>
+
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="path/to/previewtube.js"></script>
+	```
+
+3. Call the PreViewTube plugin:
+
+	```html
+	<script>
+	  $(document).ready(function(){
+	  
+	  				$('.preViewTube').PreViewTube();
+					
+					// or
+	
+					$('.preViewTube-constant').PreViewTube({
+						'interval' : 500,
+						'mode' : 'constant'
+					});
+	
+	  });
+	</script>
+	```
+
+## Options
+
+The plugin has two options `mode` and `interval` both are optional.
+
+####Interval 
+The animation rate in `ms`  
+Default: `500`ms
+
+####Mode 
+How the animation should run 
+`constant` or `hover`  
+Default : `hover`
+
+## ToDo
+
+Could be improved, might get around to it someday, sharing it is a start eh !
---
+++ previewtube.js
@@ -0,0 +1,87 @@
+/*
+ *	PreViewTube, A small jQuery plugin for animating YouTube thumbnails.
+ *	Intructions: TODO
+ *	Source:
+ *	By: Keith o'Faolain', http://vool.ie
+ *	Version: 1.0
+ *	Updated: March 21st, 2012
+ * 
+ *  Released under the MIT license.
+ *
+ */
+
+(function($) {
+
+	$.fn.PreViewTube = function(opts) {
+
+		//Defaults
+		var settings = $.extend({
+			'interval' : 500,
+			'mode' : 'hover'
+		}, opts);
+
+		//Iterate over matched elements
+		return this.each(function() {
+
+			var $this = $(this);
+
+			var loop;
+
+			// get the image source
+			var imgSrc = $this.attr("src");
+			// get the image filename
+			var fileName = imgSrc.match(/(\w*)\.jpg$/);
+
+			// get the imgae number
+			if(fileName[1] == 'default') {
+				// if default set to 1
+				var num = 1;
+			} else {
+				var num = parseInt(fileName[1]);
+			}
+
+			// declare the loop
+			var infiniteLoop = null;
+
+			// check for mode [ hover || constant ]
+			if(settings.mode == 'constant') {
+
+				loop = setInterval(function() {
+					// loop - one, two, trí
+					if(num == 3) {
+						num = 1;
+					} else {
+						num++;
+					}
+					// set the image source on the element
+					$this.attr("src", imgSrc.replace(/(\d\.jpg|\w*\.jpg)$/, +num + '.jpg'));
+
+				}, settings.interval);
+				// interval/delay loop
+
+			} else {// hover mode
+
+				$this.hover(function() {
+
+					loop = setInterval(function() {
+						// loop - one, two, trí
+						if(num == 3) {
+							num = 1;
+						} else {
+							num++;
+						}
+						// set the image source on the element
+						$this.attr("src", imgSrc.replace(/(\d\.jpg|\w*\.jpg)$/, +num + '.jpg'));
+
+					}, settings.interval);
+
+				}, function() {// mouse out
+					// stop loop
+					clearInterval(loop);
+
+				});
+			}
+
+		});
+	};
+})(jQuery);
